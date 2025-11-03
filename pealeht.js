6162b7797044601426a7afca8cc51d90095416d3
Oliver <oliveriida172@gmail.com>
2025-11-03 11:52:27 +0200

    pealehe js loogika õige

---
+++ pealeht.js
@@ -0,0 +1,3 @@
+function goToPage(url) { // defineerime funktsiooni nimega goToPage, mis võtab argumendiks vahelehe URL-i
+      window.location.href = url; // käsk, mis suunab kasutaja sellele URL-ile, millele vastav kast viitab
+}
